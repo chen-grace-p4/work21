4ba782f054caa13411e5b3d88748f25fe2d2fccb
Grace Chen <gchen@Graces-MacBook-Air.local>
2021-12-14 16:21:33 -0500

    added some comments

--- basic_server.c
+++ basic_server.c
@@ -8,11 +8,13 @@ int main() {
 
   //from_client = server_handshake( &to_client );
 
+  //after client disconnect handshake starts again
   while(1) {
     //handshake
     from_client = server_handshake( &to_client );
-
+    //continuously recieves data, process, sends back
     while(1) {
+      //recieve data
       char input[100];
       int r = read(from_client, input, sizeof(input));
       if (r == 0) break;
@@ -21,6 +23,7 @@ int main() {
       input[strlen(input)-1] = '\0';
       strcat(input, "manipulated");
 
+      //sends back
       write(to_client, input, sizeof(input));
     }
   }
